7718d2770e91bdb89b9696e0b6553869b01bfc06
clarkda <dusty@clarkda.com>
2014-04-15 17:24:07 -0500

    initial commit

---
+++ actions/PingGithub/index.js
@@ -0,0 +1,33 @@
+var Action = require('../../lib/action.js')
+  , log = require('logule').init(module, 'ping_github')
+  , ping = require("ping")
+  , util = require('util');
+
+/**
+ *
+ * @constructor
+ */
+
+var PingGithub = function() {
+  this.actionName = 'PingGithub';
+  // ..
+}
+util.inherits(PingGithub, Action);
+
+/**
+ * is github.com up?
+ */
+
+PingGithub.prototype.execute = function() {
+  var hosts = ['github.com'];
+  hosts.forEach(function(host){
+    ping.sys.probe(host, function(isAlive){
+      var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+      log.info(msg);
+    });
+  });
+}
+
+// ..
+
+module.exports = PingGithub
