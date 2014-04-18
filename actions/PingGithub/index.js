67671dbf3d829d04e2bb67076c3b698f168e29c4
clarkda <dusty@clarkda.com>
2014-04-18 14:41:21 -0500

    reformating, cleaning

--- actions/PingGithub/index.js
+++ actions/PingGithub/index.js
@@ -1,6 +1,6 @@
 var Action = require('../../lib/plugin.js')
   , log = require('logule').init(module, 'ping_github')
-  , ping = require("ping")
+  , ping = require('ping')
   , util = require('util');
 
 // ------------------------------------------------------------------------------------
@@ -10,7 +10,7 @@ var Action = require('../../lib/plugin.js')
 var PingGithub = function() {
   this.actionName = 'PingGithub';
   // ..
-}
+};
 util.inherits(PingGithub, Action);
 
 /**
@@ -19,14 +19,12 @@ util.inherits(PingGithub, Action);
 
 PingGithub.prototype.execute = function() {
   var hosts = ['github.com', 'pages.github.com'];
-  hosts.forEach(function(host){
-    ping.sys.probe(host, function(isAlive){
+  hosts.forEach(function(host) {
+    ping.sys.probe(host, function(isAlive) {
       var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
       log.info(msg);
     });
   });
-}
+};
 
-// ..
-
-module.exports = PingGithub
+module.exports = PingGithub;
